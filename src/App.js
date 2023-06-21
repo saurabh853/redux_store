9fbea517b8fbaf61720e63a52c49526fbd89b9db
Saurabh Tiwari <saurabhtiwari9825@gmail.com>
2023-06-21 22:38:14 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+// import './App.css';
+import Products from './components/Products';
+import NavBar from './components/Navbar';
+import Cart from './components/Cart';
+
+function App() {
+  return (
+    <div className="bg-gray-100">
+      <NavBar />
+      <div className="my-0 mx-auto p-4 max-w-[140rem] md:p-12">
+        <Products />
+      </div>
+      <Cart />
+    </div>
+  );
+}
+
+export default App;
